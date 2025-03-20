f68c19910b23109b643096d84a318c92af0256cf
taras <taras@mail.com>
2025-03-20 23:28:07 +0200

    Implememted infinity scroll

--- src/components/pages/QuizCatalogPage.tsx
+++ src/components/pages/QuizCatalogPage.tsx
@@ -2,15 +2,21 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../models/RootState.ts";
 import "./QuizCatalogPage.scss";
+import { addQuiz } from "../../Utils/Redux/QuizCatalogReducer.ts";
 import { useNavigate } from "react-router-dom";
 import { clearAnswers } from "../../Utils/Redux/QuizAttemptReducer.ts";
 import QuizCard from "../quiz/QuizCard.tsx";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+const QUIZZES_PER_LOAD = 4;
 
 const QuizCatalog = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
-    const quizzes = useSelector((state: RootState) => state.quizzes.allIds);
+    const quizzesId = useSelector((state: RootState) => state.quizzes.allIds);
     const quizById = useSelector((state: RootState) => state.quizzes.byId);
+    const quizzes = quizzesId.map(quizId => quizById[quizId]).filter(quiz => !quiz.isHidden)
+
     const [activeMenuId, setActiveMenuId] = useState<string | null>(null);
 
     function handleClick(quizId: string) {
@@ -22,7 +28,7 @@ const QuizCatalog = () => {
     }
 
     function quizStartHandleClick(quizId: string) {
-        dispatch(clearAnswers())
+        dispatch(clearAnswers());
         navigate(`StartQuiz/${quizId}`);
     }
 
@@ -33,23 +39,32 @@ const QuizCatalog = () => {
                 <div className="quiz-card add-card" onClick={quizSetUpHandleClick}>
                     <span className="plus">+</span>
                 </div>
+                <InfiniteScroll
+                        dataLength={quizzes.length}
+                        next={() => {
+                            dispatch(addQuiz({id: crypto.randomUUID(), description:"fdff", name:"Fdfdf"}))
+                        }}
+                        hasMore={true}
+                        loader={<h4>Loading...</h4>}
+                        endMessage={<p style={{ textAlign: "center" }}>No more quizzes</p>}
+                    >
                 {quizzes.length === 0 ? (
                     <p>No quizzes available</p>
                 ) : (
-                    quizzes.map((quizId) => {
-                        const quiz = quizById[quizId];
+                    quizzes.map((quiz) => {
                         return (
                             <QuizCard
-                                key={quizId}
-                                quizId={quizId}
+                                key={quiz.id}
+                                quizId={quiz.id}
                                 quiz={quiz}
-                                isMenuActive={activeMenuId === quizId}
-                                onOptionsClick={() => handleClick(quizId)}
-                                onCardClick={() => quizStartHandleClick(quizId)}
+                                isMenuActive={activeMenuId === quiz.id}
+                                onOptionsClick={() => handleClick(quiz.id)}
+                                onCardClick={() => quizStartHandleClick(quiz.id)}
                             />
                         );
                     })
                 )}
+                </InfiniteScroll>
             </div>
         </div>
     );
